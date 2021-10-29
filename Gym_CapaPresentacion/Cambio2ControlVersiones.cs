40aff5beb559f076d412407cbebb1b066faaf1ad
Roberto-sc <luis.solano@ucrso.info>
2021-10-29 14:33:55 -0600

    Segundo cambio

---
+++ Gym_CapaPresentacion/Cambio2ControlVersiones.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gym_CapaPresentacion
+{
+    class Cambio2ControlVersiones
+    {
+
+        // Cambio de código #2 
+
+
+        public Cambio2ControlVersiones() { 
+        
+        
+        
+        
+        }
+    }
+}
