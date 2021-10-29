841ea7379bc73685d9fb2a5fa53d80c3441d87cd
Roberto-sc <luis.solano@ucrso.info>
2021-10-29 15:10:46 -0600

    Cambio de correccion

---
+++ Gym_CapaPresentacion/CambioDeCorreccion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gym_CapaPresentacion
+{
+    class CambioDeCorreccion
+    {
+
+
+        // Cambio de código en la rama de corrección
+
+
+
+        public CambioDeCorreccion()
+        {
+
+
+
+        }
+
+
+
+    }
+}
