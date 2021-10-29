d23c1cc1d9795f2cfc3f80af42dc22db0c5679f9
Roberto-sc <luis.solano@ucrso.info>
2021-10-29 14:22:55 -0600

    Primer cambio

---
+++ Gym_CapaPresentacion/Cambio1ControlVersiones.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gym_CapaPresentacion
+{
+    class Cambio1ControlVersiones
+    {
+        // Cambio de código en la rama de función # 1
+
+
+
+        public Cambio1ControlVersiones() { 
+        
+        
+        
+        }
+
+
+
+
+
+
+
+    }
+}
